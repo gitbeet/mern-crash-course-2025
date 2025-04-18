dfac8b1c1067509d95557759715641b6a41b1b7a
gitbeet <nikig715@gmail.com>
2025-04-18 20:19:04 +0200

    PORT env variable, delete controller id check and error message changed

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ import express, { json } from "express";
 import { connectDb } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 app.use(json());
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDb();
-  console.log("Server started on port 5000");
+  console.log(`Server started on port ${PORT}`);
 });
